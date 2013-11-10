22cb4592146384c6202f53e993307bcb6cd12f4a
Haoqi Zhang <hqz@mit.edu>
2013-11-09 17:34:34 -0800

    hi

---
+++ face-stream/stream.js
@@ -0,0 +1,57 @@
+var query = "funny";
+var photostream = [];
+
+var t = 0;
+var interval = 2000;
+var currentID = 0;
+
+var binary = true;
+
+$(function() {
+	init();
+	$('#start').click(function() {
+		query = $('#keyword').val();
+
+		    if ($(this).text() == 'Start'){
+			    t = setTimeout(loadNew, interval);
+			    $(this).text('Stop');
+			}
+		    else
+			{
+			    clearTimeout(t);
+			    $(this).text('Start');
+			}
+		});
+	});
+
+function init(){
+    kittenGenerator.requestKittens();
+}
+
+function loadInitial(photos){
+    photostream = photos;
+    for(var i = 0; i < 5; i++){
+	var stuff = $('<li></li>');
+	$('#photostream').prepend(stuff);
+    }
+}
+
+function getID() {
+    return currentID;
+}
+
+function loadNew() {
+    $('li:first').addClass('opaque');
+    var current = currentID;
+    if(binary){
+	var stuff = $('<div></div>').append(photostream[current]);
+	stuff.append(photostream[current+1]);
+	smoothAdd('photostream', stuff);
+	currentID+=2;
+    }else{
+	smoothAdd('photostream', photostream[current]);
+	currentID+=1;
+    }
+    t = setTimeout(loadNew, interval);
+}
+	
